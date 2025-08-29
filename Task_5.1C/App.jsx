bfe724afcf19959142a3de146c7dd73c301aaea1
SwastikSharma01 <swastiksharma300@gmail.com>
2025-08-29 20:05:48 +0530

    Add files via upload

---
+++ Task_5.1C/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+import Post_type from './components/posttype'
+import QuestionForm from './components/QuestionForm'
+import ArticleForm from './components/ArticleForm'
+function App() {
+
+  return (
+    <>
+      <div>
+
+        <Post_type/>
+        <br />
+        {/* <QuestionForm/> */}
+        {/* <ArticleForm/> */}
+
+      </div>
+    </>
+  )
+}
+
+export default App
